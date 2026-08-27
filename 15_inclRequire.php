7e94df6ceed2297ce922203cefcc07054782edec
aayramudzf <aayra.mudzaffarshah@student.gmi.edu.my>
2026-08-27 23:11:18 +0800

    Add 15_includeRequire - Include and Require files

---
+++ 15_inclRequire.php
@@ -0,0 +1,24 @@
+<?php
+
+//include('ninja.php');
+//require('ninja.php');
+
+require 'ninjas.php';
+include 'ninja.php';
+
+echo 'end of php';
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Tutorials</title>
+</head>
+<body>
+
+    <>php include('content.php'); ?>
+
+</body>
+</html>
