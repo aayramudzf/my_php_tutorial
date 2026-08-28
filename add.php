d4b2fa4a8cb6e99d907e09db1895cf94b7dc1a7a
aayramudzf <aayra.mudzaffarshah@student.gmi.edu.my>
2026-08-28 11:07:59 +0800

    Video 19 - Basic form validation part1

--- add.php
+++ add.php
@@ -1,16 +1,28 @@
 <?php
 
-   // if(isset($_GET['submit'])){
-   //     echo $_GET['email'];
-   //     echo $_GET['title'];
-   //     echo $_GET['ingredients'];
-   // }
-
     if(isset($_POST['submit'])){
+      
+       //check email
+       if(empty($_POST['email'])){
+        echo 'An email is required <br />';
+       } else {
         echo htmlspecialchars($_POST['email']);
+       }
+
+         //check title
+       if(empty($_POST['title'])){
+        echo 'A title is required <br />';
+       } else {
         echo htmlspecialchars($_POST['title']);
+       }
+
+         //check ingredients
+       if(empty($_POST['ingredients'])){
+        echo 'At least one ingredient is required <br />';
+       } else {
         echo htmlspecialchars($_POST['ingredients']);
-    }
+       }
+    } // end of POST check
 ?>
 
 <!DOCTYPE html>
